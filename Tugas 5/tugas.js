3cad0d2b9ac009003dcb5e4999cfe1bd07f0530b
Tasya <119835635+dotasya@users.noreply.github.com>
2023-11-02 22:43:09 +0700

    Js

---
+++ Tugas 5/tugas.js
@@ -0,0 +1,15 @@
+//Tugas 1
+function add(a, b) {
+  console.log(a - b);
+}
+add(10, 5);
+
+//tugas 2
+function add(angka) {
+  angka = 20;
+  if (angka > 10) {
+    console.log("angka lebih dari 10");
+  } else {
+    console.log("angka tidak lebih dari 10");
+  }
+}
